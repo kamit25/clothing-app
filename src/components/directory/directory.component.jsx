321653ab60a9fed000d34833e5b624e02bcd9839
kami25 <amitkumar.sah403@gmail.com>
2020-02-10 18:26:46 +0530

    router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-
 import "./directory.styles.scss";
-import MenuItem from '../menu-item/menu-item.component';
+import MenuItem from "../menu-item/menu-item.component";
 class Directory extends React.Component {
   constructor() {
     super();
@@ -43,16 +42,14 @@ class Directory extends React.Component {
     };
   }
 
-  render(){
-      return (
-          <div className = "directory-menu">
-          {
-              this.state.sections.map( ({title,imageUrl,id,size}) =>(
-                  <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}/>
-              ))
-          }
-          </div>
-      )
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id , ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
